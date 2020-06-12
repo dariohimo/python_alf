4bc1ffba0857979470faeea1ea19d9b051802eda
dariohimo <dariohimo@gmail.com>
2020-06-12 17:10:50 -0500

    name upper and len command

---
+++ name_long.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+name = input("What's your name? " )
+
+#num = int(input("Digit one number"))
+
+#print("\n")
+print (f"Your name is: {name.upper()}, and your long name: {len(name)}\n")   
